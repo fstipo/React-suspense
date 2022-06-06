79207041cc4de2c92e86a0e6b1d1b511c3409a63
fstipo <fstipanov@gmail.com>
2022-06-06 16:21:19 +0200

    fetch data

--- src/App.js
+++ src/App.js
@@ -1,5 +1,29 @@
+const fetchUser = async () => {
+  const response = await fetch('https://jsonplaceholder.typicode.com/ussers/1');
+
+  if (response.status !== 200) {
+    throw new Error("⛔ Can't fetch the data");
+  }
+
+  const data = await response.json();
+  return data;
+};
+
+fetchUser()
+  .then((data) => {
+    console.log('Resolved:', data);
+  })
+  .catch((err) => console.log('Rejected:', err.message));
+
 function App() {
-  return <div className="m-3 display-2">Hello 🤞</div>;
+  return (
+    <div className="container my-5 text-danger">
+      {/* <Suspense fallback={<h1>Loading user...</h1>}>
+        <ProfileDetails />
+      </Suspense> */}
+      <h1>Hello 🐶</h1>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/ProfileDetails.js
@@ -0,0 +1,19 @@
+// import React from 'react';
+
+// const resource = fetchData();
+
+// const ProfileDetails = () => {
+//   const user = resource.user.read();
+//   return (
+//     <div className="card card-body my-3">
+//       <h1 className="large text-primary">{user.name}</h1>
+//       <ul>
+//         <li>Username:</li>
+//         <li>Email:</li>
+//         <li>City:</li>
+//       </ul>
+//     </div>
+//   );
+// };
+
+// export default ProfileDetails;
